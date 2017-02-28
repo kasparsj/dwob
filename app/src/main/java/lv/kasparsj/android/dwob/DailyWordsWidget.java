6882dae21a767bd8ef0632c69f7c06b559e412a1
Kaspars Jaudzems <kasparsj@gmail.com>
2017-02-28 22:40:52 +0200

    DailyWordsWidget refactoring updated targetSdkVersion to 25

--- app/src/main/java/lv/kasparsj/android/dwob/DailyWordsWidget.java
+++ app/src/main/java/lv/kasparsj/android/dwob/DailyWordsWidget.java
@@ -1,5 +1,6 @@
 package lv.kasparsj.android.dwob;
 
+import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -8,16 +9,22 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.widget.FrameLayout;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
 import lv.kasparsj.android.dwob.model.DailyWords;
+import lv.kasparsj.android.util.Objects;
 import lv.kasparsj.android.widget.AutoFitTextView;
 
-public class DailyWordsWidget extends AppWidgetProvider {
+public class DailyWordsWidget extends AppWidgetProvider
+{
+    public static final String OPTION_APPWIDGET_MIN_WIDTH = "appWidgetMinWidth";
+    public static final String OPTION_APPWIDGET_MIN_HEIGHT = "appWidgetMinHeight";
 
     @Override
     public void onEnabled(Context context) {
@@ -73,61 +80,116 @@ public class DailyWordsWidget extends AppWidgetProvider {
         }
         else if (translation.length() > 0) {
             for (int appWidgetId : appWidgetIds) {
-                update(context, manager, appWidgetId);
+                Point size = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    size = getWidgetSize(manager, appWidgetId);
+                    if (size.x <= 0 || size.y <= 0) {
+                        continue;
+                    }
+                }
+                RemoteViews updateViews = createUpdateViews(context, size);
+                manager.updateAppWidget(appWidgetId, updateViews);
             }
         }
     }
 
-    protected void update(Context context, AppWidgetManager manager, int appWidgetId) {
-        // Retrieve latest translation
-        DailyWords dailyWords = DailyWords.getInstance();
-        Resources r = context.getResources();
-        String text = r.getString(R.string.widget_error);
-        String translation = dailyWords.getTranslated();
-        if (translation.length() > 0) {
-            text = translation.trim();
-        }
-        // Build an update that holds the updated widget contents
-        RemoteViews updateViews;
-        updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_words);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            Bundle options = manager.getAppWidgetOptions(appWidgetId);
-            int width = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
-            int height = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
-            AutoFitTextView autoFitTextView = new AutoFitTextView(context);
-            autoFitTextView.measure(width, height);
-            autoFitTextView.layout(0, 0, width, height);
-            autoFitTextView.setMaxLines(getMaxLines());
-            autoFitTextView.setText(text);
-            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-            autoFitTextView.draw(new Canvas(bitmap));
-            updateViews.setImageViewBitmap(R.id.words, bitmap);
+    protected RemoteViews createUpdateViews(Context context, Point size) {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_words);
+        if (size != null) {
+            Bitmap bitmap = renderTextView(context, size);
+            remoteViews.setImageViewBitmap(R.id.words, bitmap);
         }
         else {
-            // Detect numLines to display
-            String[] lines = text.split("\r\n|\r|\n");
-            if (lines.length > getMaxLines() && text.contains("\n\n")) {
-                String[] parts = text.split("\n\n");
-                text = parts[0].trim()+"...";
-                lines = text.split("\r\n|\r|\n");
-            }
-            // Measure text width, and alter numLines accordingly
-            TextView textView = new TextView(context);
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDefaultTextSize(lines.length));
-            float width = 80 * r.getDisplayMetrics().density * 4;
-            float padding = r.getDimension(R.dimen.widget_padding);
-            float margin = r.getDimension(R.dimen.widget_margin);
-            float lineWidth = (width - padding*2 - margin*2);
-            while (lines.length < countTextViewLines(textView, lines, lineWidth, r.getDisplayMetrics().density)) {
-                textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, (textView.getTextSize()-.5f));
-            }
-            updateViews.setTextViewText(R.id.words, text);
-            updateViews.setFloat(R.id.words, "setTextSize", textView.getTextSize());
+            updateTextView(context, remoteViews);
         }
-        // setOnClickPendingIntent
+        PendingIntent pendingIntent = getOnClickPendingIntent(context);
+        remoteViews.setOnClickPendingIntent(R.id.words, pendingIntent);
+        return remoteViews;
+    }
+
+    protected Bitmap renderTextView(Context context, Point size) {
+        AutoFitTextView autoFitTextView = new AutoFitTextView(context);
+        autoFitTextView.measure(size.x, size.y);
+        autoFitTextView.layout(0, 0, size.x, size.y);
+        autoFitTextView.setLayoutParams(new FrameLayout.LayoutParams(size.x, size.y));
+        autoFitTextView.setMaxLines(getMaxLines());
+        autoFitTextView.setTextSize(500);
+        autoFitTextView.enableSizeCache(false);
+        autoFitTextView.setText(getText(context));
+        Bitmap bitmap = Bitmap.createBitmap(size.x, size.y, Bitmap.Config.ARGB_8888);
+        autoFitTextView.draw(new Canvas(bitmap));
+        return bitmap;
+    }
+
+    protected void updateTextView(Context context, RemoteViews remoteViews) {
+        Resources r = context.getResources();
+        String text = getText(context);
+        String[] lines = text.split("\r\n|\r|\n");
+        if (lines.length > getMaxLines() && text.contains("\n\n")) {
+            String[] parts = text.split("\n\n");
+            text = parts[0].trim()+"...";
+            lines = text.split("\r\n|\r|\n");
+        }
+        // Measure text width, and alter numLines accordingly
+        TextView textView = new TextView(context);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getDefaultTextSize(lines.length));
+        float width = 80 * r.getDisplayMetrics().density * 4;
+        float padding = r.getDimension(R.dimen.widget_padding);
+        float margin = r.getDimension(R.dimen.widget_margin);
+        float lineWidth = (width - padding*2 - margin*2);
+        while (lines.length < countTextViewLines(textView, lines, lineWidth, r.getDisplayMetrics().density)) {
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, (textView.getTextSize()-.5f));
+        }
+        remoteViews.setTextViewText(R.id.words, text);
+        remoteViews.setFloat(R.id.words, "setTextSize", textView.getTextSize());
+    }
+
+    protected String getText(Context context) {
+        DailyWords dailyWords = DailyWords.getInstance();
+        String translation = dailyWords.getTranslated();
+        if (translation.length() > 0) {
+            return translation.trim();
+        }
+        return context.getResources().getString(R.string.widget_error);
+    }
+
+    protected PendingIntent getOnClickPendingIntent(Context context) {
         Intent defineIntent = new Intent(context, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, defineIntent, 0);
-        updateViews.setOnClickPendingIntent(R.id.words, pendingIntent);
+        return PendingIntent.getActivity(context, 0, defineIntent, 0);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        // Handle TouchWiz
+        if (Objects.equals(intent.getAction(), "com.sec.android.widgetapp.APPWIDGET_RESIZE")) {
+            AppWidgetManager manager = AppWidgetManager.getInstance(context);
+            int appWidgetId = intent.getIntExtra("widgetId", 0);
+            int widgetSpanX = intent.getIntExtra("widgetspanx", 0);
+            int widgetSpanY = intent.getIntExtra("widgetspany", 0);
+            if (appWidgetId > 0 && widgetSpanX > 0 && widgetSpanY > 0) {
+                Bundle newOptions = new Bundle();
+                newOptions.putInt(OPTION_APPWIDGET_MIN_HEIGHT, widgetSpanY * 74);
+                newOptions.putInt(OPTION_APPWIDGET_MIN_WIDTH, widgetSpanX * 74);
+                onAppWidgetOptionsChanged(context, manager, appWidgetId, newOptions);
+            }
+        }
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged (Context context, AppWidgetManager manager, int appWidgetId, Bundle newOptions) {
+        int width = newOptions.getInt(OPTION_APPWIDGET_MIN_WIDTH);
+        int height = newOptions.getInt(OPTION_APPWIDGET_MIN_HEIGHT);
+        RemoteViews updateViews = createUpdateViews(context, new Point(width, height));
         manager.updateAppWidget(appWidgetId, updateViews);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    protected Point getWidgetSize(AppWidgetManager manager, int appWidgetId) {
+        Bundle options = manager.getAppWidgetOptions(appWidgetId);
+        int width = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        int height = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+        return new Point(width, height);
     }
 }
